a543df6e9490b95fddce05b4512eff6f3525c68a
Steve Edwards <sedward5@emich.edu>
2024-11-07 16:49:15 +0000

    More linting

--- custom_components/spc-outlook/sensor.py
+++ custom_components/spc-outlook/sensor.py
@@ -1,6 +1,13 @@
 """Platform for sensor integration."""
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from homeassistant.core import HomeAssistant
+    from homeassistant.helpers.entity_platform import AddEntitiesCallback
+    from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
 import requests
 from homeassistant.components.sensor import (
     SensorDeviceClass,
@@ -8,9 +15,6 @@ from homeassistant.components.sensor import (
     SensorStateClass,
 )
 from homeassistant.const import UnitOfTemperature
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from shapely.geometry import Point, shape
 
 
@@ -44,31 +48,35 @@ def getspcoutlook():
     output = {}
     location = Point(-83, 42) # add your lon lat here
     for day in range(1, 4):
-        url = "https://www.spc.noaa.gov/products/outlook/day" + str(day) + "otlk_cat.lyr.geojson"
+        url = "https://www.spc.noaa.gov/products/outlook/day" + \
+            str(day) + "otlk_cat.lyr.geojson"
         result = False
         resp = requests.get(url=url, timeout=10)
         data = resp.json()
         for feature in data["features"]:
             polygon = shape(feature["geometry"])
             if polygon.contains(location):
-                output['cat_day'+str(day)] = feature["properties"]["LABEL2"]
+                output["cat_day"+str(day)] = feature["properties"]["LABEL2"]
                 result = True
         if result and day < 3:
-            torn_url = "https://www.spc.noaa.gov/products/outlook/day" + str(day)+ "otlk_torn.lyr.geojson"
+            torn_url = "https://www.spc.noaa.gov/products/outlook/day" + \
+                str(day)+ "otlk_torn.lyr.geojson"
             resp = requests.get(url=torn_url, timeout=10)
             data = resp.json()
             for feature in data["features"]:
                 polygon = shape(feature["geometry"])
                 if polygon.contains(location):
                     print("Tornado risk: ", feature["properties"]["LABEL2"])
-            hail_url = "https://www.spc.noaa.gov/products/outlook/day"+str(day)+"otlk_hail.lyr.geojson"
+            hail_url = "https://www.spc.noaa.gov/products/outlook/day"+str(day)+\
+                "otlk_hail.lyr.geojson"
             resp = requests.get(url=hail_url, timeout=10)
             data = resp.json()
             for feature in data["features"]:
                 polygon = shape(feature["geometry"])
                 if polygon.contains(location):
                     print("Hail risk: ", feature["properties"]["LABEL2"])
-            wind_url = "https://www.spc.noaa.gov/products/outlook/day"+str(day)+"otlk_wind.lyr.geojson"
+            wind_url = "https://www.spc.noaa.gov/products/outlook/day"+str(day)+\
+                "otlk_wind.lyr.geojson"
             resp = requests.get(url=wind_url, timeout=10)
             data = resp.json()
             for feature in data["features"]:
